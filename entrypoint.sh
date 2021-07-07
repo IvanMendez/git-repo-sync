39fbe838255126065d544367320017e7a5bea843
Ivan Mendez <ivmenddez@gmail.com>
2021-07-07 18:51:44 -0500

    use mirror

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@ git remote add target http://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${IN
 
 case "${GITHUB_EVENT_NAME}" in
     push)
-        git push --mirror target
+        git push -f --mirror target
         ;;
     delete)
         git push -d target ${GITHUB_EVENT_REF}
